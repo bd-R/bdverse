2d634de114234c3a2c8d76e7e5c198d274aa3760
Thiloshon <thiloshon@gmail.com>
2019-10-20 12:21:25 +0530

    text_col fix

--- R/zzz.R
+++ R/zzz.R
@@ -87,3 +87,19 @@ msg <- function(..., startup = FALSE) {
     message(text_col(...))
   }
 }
+
+text_col <- function(x) {
+  # If RStudio not available, messages already printed in black
+  if (!rstudioapi::isAvailable()) {
+    return(x)
+  }
+  
+  if (!rstudioapi::hasFun("getThemeInfo")) {
+    return(x)
+  }
+  
+  theme <- rstudioapi::getThemeInfo()
+  
+  if (isTRUE(theme$dark)) crayon::white(x) else crayon::black(x)
+  
+}
